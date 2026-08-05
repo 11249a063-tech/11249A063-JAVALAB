e9681f292e1754f95513782d8795b6ca99981b25
11249a063-tech <11249a063@kanchiuniv.ac.in>
2026-08-05 12:06:57 +0530

    Add files via upload

---
+++ SquareDemo.java
@@ -0,0 +1,8 @@
+class SquareDemo{
+    static void Square(int n){
+        System.out.println("square of "+n+"="+(n*n));
+    }
+    public static void main(String[] args) {
+        SquareDemo.Square(7);
+    }
+}
