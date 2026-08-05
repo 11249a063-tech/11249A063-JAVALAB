0681a632ea991d7a0b1589afd3bc1701bf7c872f
11249a063-tech <11249a063@kanchiuniv.ac.in>
2026-08-05 11:53:28 +0530

    Add files via upload

---
+++ java applications.java
@@ -0,0 +1,13 @@
+Class student{
+    String name="Rahul";
+    int age=20;
+    Void display(){
+        System.out.Println(Name:"+name);
+        System.out.Println(Age:"+age);
+    }
+    Public Static Void main(String[]args){
+        Students=new Student();
+        s.display();
+    }
+}
+
